24c0a5c5601030690cfba5024950507c1e09520c
PeterSolution <piotrwolff@op.pl>
2024-04-14 12:50:25 +0200

    first

---
+++ common/defines/krl_defines.lua
@@ -0,0 +1,99 @@
+-- no land division training xp
+NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0
+
+-- can embargo anytime
+NDefines.NDiplomacy.EMBARGO_THREAT_THRESHOLD = -1   
+-- Target-generated threat threshold to allow embargo (affected by modifiers)
+
+
+-- osłabiony logistic strike damage 4x
+-- zmiana /4 
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_DAMAGE_FACTOR = 0.0675 
+-- zmiana *4
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_INFRA_DAMAGE_SPILL_FACTOR = 0.0064 -- Portion of truck damage to additionally deal to infrastructure 
+-- zmiana /4 
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_FACTOR = 0.010 
+-- zmiana *4
+NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0.024 -- Portion of train damage to additionally deal to railways 
+
+
+--Convoy Prio
+NDefines.NNavy.NAVAL_INVASION_PRIORITY = 1								-- Default convoy priority for naval invasions
+NDefines.NNavy.NAVAL_TRANSFER_PRIORITY = 1								-- Default convoy priority for naval transports
+NDefines.NNavy.SUPPLY_PRIORITY = 2								    	-- Default convoy priority for supplying units via sea
+NDefines.NNavy.RESOURCE_ORIGIN_PRIORITY = 3								-- Default convoy priority for resources shipped internally
+NDefines.NNavy.RESOURCE_EXPORT_PRIORITY = 4								-- Default convoy priority for export trade
+NDefines.NNavy.RESOURCE_LENDLEASE_PRIORITY = 5                          -- Default convoy priority for export lend lease
+
+-- performance:
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.25, 0.19, 0.1, 0.06, 0.0 } -- Must be 5
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 13 							    -- WAS 48 | drastically cuts down on save file sizes after WW2 starts and well into barbarossa
+NDefines.NGame.MESSAGE_TIMEOUT_DAYS = 14								-- WAS 60 | less messages lying around at the top of your screen
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 30
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0                     -- Removed for game stability/reducing chance of desync
+NDefines.NGame.MISSION_REMOVE_FROM_INTERFACE_DEFAULT = 3 -- Default days before a mission is removed from the interface after having failed or completed
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 20 -- Performance enhancer. --TW/WTT
+
+-- special forces
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 0.00						    
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.00       
+
+-- volunteers
+NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.0			    -- Volunteer shit to prevent 2w spam
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.0				    -- Volunteer shit to prevent 2w spam
+NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 1				    -- Volunteer shit to prevent 2w spam
+
+-- Licenses
+NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0					-- Free license
+NDefines.NProduction.MIN_LICENSE_ACTIVE_DAYS = 1                        -- Free license
+NDefines.NProduction.BASE_LICENSE_IC_COST = 0						    -- Base IC cost for lended license
+
+-- free templates and design
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0				
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 0
+NDefines.NIndustrialOrganisation.DESIGN_TEAM_CHANGE_XP_COST = 1								-- Flat cost added to the XP cost of a new equipment design. If 0, cost is entirely disabled.
+
+-- AI
+NDefines.NAI.MINIMUM_EQUIPMENT_TO_ASK_LEND_LEASE = -1949731	-- WAS 100, changed to prevent exploiting AI for ~150k guns at the start of WW2 | AI will accept to lend lease this equipment only if our stockpile is less than that.
+NDefines.NAI.DIPLOMACY_ACCEPT_VOLUNTEERS_BASE = 100		-- Base value of volunteer acceptance (help is welcome)
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_BASE = 100
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_TRASHHOLD = 0
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_PENALTY = 0
+NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROLLED = 0
+NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROL_DIFF = 0
+
+-- QOL
+NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000
+NDefines.NMilitary.MAX_AIR_EXPERIENCE  = 1000
+
+-- other:
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 10000000
+
+-- Anti-Troll spy actions
+NDefines.NOperatives.BOOST_IDEOLOGY_NATIONAL_COVERAGE_FACTOR = 0
+NDefines.NOperatives.BOOST_IDEOLOGY_MAX_DRIFT_BY_OPERATIVE = 0
+NDefines.NOperatives.BOOST_IDEOLOGY_DRIFT_STACKING_FACTOR = 0
+NDefines.NOperatives.BOOST_IDEOLOGY_DAILY_XP_GAIN = 0
+NDefines.NOperatives.OPERATIVE_BASE_PROPAGANDA_POWER = 0
+NDefines.NOperatives.PROPAGANDA_OPERATIVE_STACKING_FACTOR = 0
+NDefines.NOperatives.PROPAGANDA_COUNTRY_STACKING_FACTOR = 0
+NDefines.NOperatives.PROPAGANDA_DAILY_XP_GAIN = 0
+NDefines.NOperatives.MIN_NATIONAL_COVERAGE_FOR_PROPAGANDA = 100
+NDefines.NOperatives.PROPAGANDA_SUB_NETWORK_STRENGTH_FACTOR = 0
+NDefines.NOperatives.OPERATIVE_BASE_CONTROL_TRADE_DRIFT = 0
+NDefines.NOperatives.CONTROL_TRADE_STACKING_FACTOR = 0
+NDefines.NOperatives.CONTROL_TRADE_MAX_INFLUENCE = 0
+NDefines.NOperatives.CONTROL_TRADE_INFLUENCE_DAILY_DECAY = 0
+NDefines.NOperatives.CONTROL_TRADE_DAILY_XP_GAIN = 0
+NDefines.NOperatives.OPERATIVE_BASE_DIPLOMATIC_PRESSURE_AI_ACCEPTANCE_SCORE_DRIFT = 0
+NDefines.NOperatives.DIPLOMATIC_PRESSURE_MAX_AI_ACCEPTANCE_SCORE_INCREASE = 0
+NDefines.NOperatives.DIPLOMATIC_PRESSURE_MAX_TENSION_REQUIREMENTS_DECREASE = 0
+NDefines.NOperatives.DIPLOMATIC_PRESSURE_DAILY_XP_GAIN = 0
---
+++ descriptor.mod
@@ -0,0 +1,10 @@
+version="1.0.0"
+tags={
+	"Balance"
+	"Gameplay"
+	"Utilities"
+	"Translation"
+}
+name="KRL historyczny multiplayer"
+supported_version="1.14.4"
+remote_file_id="3186989065"
